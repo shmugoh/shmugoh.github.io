d008dbfd649f88755aeaaad719c5045838c4f1e5
Juan P. <juan@shmugo.co>
2024-04-29 17:41:37 -0500

    Add Social Media Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), icon()],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "es"],
-  },
+    locales: ["en", "es"]
+  }
 });
---
+++ src/config.ts
@@ -0,0 +1,54 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://shmugo.co/", // replace this with your deployed domain
+  author: "Juan",
+  desc: "hello i am shmugo",
+  title: "shmugo",
+  ogImage: "hard_work.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    icon: "simple-icons:github",
+    href: "https://github.com/shmugoh",
+    linkTitle: ` ${SITE.title} on GitHub`,
+    active: true,
+  },
+  {
+    name: "Ko-Fi",
+    icon: "simple-icons:kofi",
+    href: "https://ko-fi.com/shmugo",
+    linkTitle: ` ${SITE.title} on Ko-Fi`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    icon: "simple-icons:twitter",
+    href: "https://twitter.com/shmugo_",
+    linkTitle: ` ${SITE.title} on GitHub`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    icon: "simple-icons:gmail",
+    href: "mailto:juan@shmugoh.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+];
